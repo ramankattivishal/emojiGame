87f87576230dfe14744f310561eec160b43949ef
vishal <ramankattivishal@gmail.com>
2022-11-19 12:33:21 +0000

    app

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+import {Component} from 'react'
+
+class EmojiCard extends Component {
+  render() {
+    const {emoji, onClickEmoji} = this.props
+    const {emojiName, emojiUrl, id} = emoji
+    const onclickEmojiItem = () => {
+      onClickEmoji(id)
+    }
+    return (
+      <li className="emoji-item" onClick={onclickEmojiItem}>
+        <button type="button" className="button">
+          <img
+            key={id}
+            src={emojiUrl}
+            alt={emojiName}
+            className="emoji-image"
+          />
+        </button>
+      </li>
+    )
+  }
+}
+
+export default EmojiCard
